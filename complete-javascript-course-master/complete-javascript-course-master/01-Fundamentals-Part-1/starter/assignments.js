cad68f7cbf3c9cbc351e46ff62d24d829edfe5e9
arkytechie <40334092+arkytechie@users.noreply.github.com>
2021-04-14 20:19:31 +0200

    basic fundamentals hands-on Part-1

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,5 @@
+let country = "Denmark";
+let continent = "Europe";
+let population = "5.5 million";
+
+console.log(country, continent, population);
---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+let js = 'Amazing';
+console.log(1 + 2 + 2 + 3 + 4 + 5);
+
+let firstName = "Arkesh"
+console.log(firstName);
+
+let PI = 3.14;
+let _name = "ArkyTechie";
+console.log(_name);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
